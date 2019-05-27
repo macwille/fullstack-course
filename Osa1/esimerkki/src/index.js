8df2f943434014d74378c07d1dd7bd06c4a0b152
Macville <manninen94@gmail.com>
2019-05-27 19:02:40 +0300

    Osa 1c

--- Osa1/esimerkki/src/index.js
+++ Osa1/esimerkki/src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
-    console.log('Hello component called')
+const Hello = ({name, age}) => {
+    const bornYear = () => new Date().getFullYear() - age
+
     return (
         <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
+        <p>Hello {name}, you are {age} years old</p>
+            <p>So you were probably born {bornYear()}</p>
         </div>
     )
 }
 
-const App = () => {
+const App = (props) => {
     console.log('Main app')
     const nimi = 'Pekka'
     const ika = 10
@@ -23,6 +25,44 @@ const App = () => {
 
     )
 }
+const Display = ({counter}) =>  <div> {counter} </div>   
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = ({handleClick, text}) => (
+    <button onClick = {handleClick}>
+    {text}
+    </button>
+)
+
+const App2 = (props) => {
+    const [ counter, setCounter ] = useState(0)
+
+    const setToValue = (value) => () => {setCounter(value)}
+    return (
+        <div>
+            <Display counter = {counter}/>
+            <Button 
+            handleClick = {setToValue(counter+1)}
+            text ='plus'
+            />
+            <Button
+            handleClick = {setToValue(counter -1)}
+            text = 'minus'
+            />
+            <Button
+            handleClick = {setToValue(0) }
+            text = 'zero'
+            />
+
+        </div>
+    )
+}
+
+// App1
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// App2
+ReactDOM.render(
+    <App2 />,
+    document.getElementById('root')
+)
 
