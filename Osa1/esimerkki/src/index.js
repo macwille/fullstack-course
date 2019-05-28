d37b767a88b33126597b13f654fce318e08ef22c
Macville <manninen94@gmail.com>
2019-05-28 17:50:28 +0300

    1.7: unicafe step2

--- Osa1/esimerkki/src/index.js
+++ Osa1/esimerkki/src/index.js
@@ -25,37 +25,55 @@ const App = (props) => {
 
     )
 }
-const Display = ({counter}) =>  <div> {counter} </div>   
 
-const Button = ({handleClick, text}) => (
-    <button onClick = {handleClick}>
-    {text}
-    </button>
-)
-
-const App2 = (props) => {
-    const [ counter, setCounter ] = useState(0)
-
-    const setToValue = (value) => () => {setCounter(value)}
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used  by pressing the buttons
+            </div>
+        )
+    }
     return (
         <div>
-            <Display counter = {counter}/>
-            <Button 
-            handleClick = {setToValue(counter+1)}
-            text ='plus'
-            />
-            <Button
-            handleClick = {setToValue(counter -1)}
-            text = 'minus'
-            />
-            <Button
-            handleClick = {setToValue(0) }
-            text = 'zero'
-            />
-
+            button press history: {props.allClicks.join(' ')}
         </div>
     )
 }
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+    {text}
+    </button>
+)
+const App2 = (props) => {
+    const[left, setLeft] = useState(0)
+    const[right, setRight] = useState(0)
+    const[allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left+1)
+    }
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right +1)
+    }
+    return (
+        
+        <div>
+            
+            <div>
+                <h1> React App</h1>
+                {left}
+                <Button handleClick = {handleLeftClick} text = 'left'/>
+                <Button handleClick = {handleRightClick} text = 'right'/>
+                {right}
+                <History allClicks = {allClicks}/>
+            </div>
+        </div>
+    )
+}
+
 
 // App1
 // ReactDOM.render(<App />, document.getElementById('root'));
