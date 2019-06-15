79977ecb8ff6177dbcbb1b0f5799855ca56b7482
Macville <manninen94@gmail.com>
2019-06-15 23:04:36 +0300

    1.13 anekdootit step2

--- Osa1/anecdotes/src/index.js
+++ Osa1/anecdotes/src/index.js
@@ -3,27 +3,44 @@ import ReactDOM from 'react-dom';
 
 const App = (props) => {
     const [selected, setSelected] = useState(0)
+
     const handleVote = () => {
-
+        props.points[selected] += 1
+        console.log(props.points)
     }
+
     const handleNext = () => {
+        console.log('next anecdote clicked')
+        let nextNumber  = getRandomNumber(0, props.anecdotes.length)
+        console.log(nextNumber)
+        setSelected(nextNumber)
     }
 
-    
+    let mostVoted = points.indexOf(Math.max(...points));
+
+ 
 return (
     <div>
         <h1>Anecdote of the day</h1>
         {props.anecdotes[selected]}
         <br/>
+        has {props.points[selected]} votes
+        <br/>
         <button onClick = {handleVote}> vote </button>
         <button onClick = {handleNext}> next anecdote </button>
 
         <h1> Anecdote with the most votes</h1>
-        {props.anecdotes[mostVotes]}
+        {props.anecdotes[mostVoted]}
+        <br/>
+        has {props.points[mostVoted]} points
+        
+
     </div>
 )
+}
 
-
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max-min) + min);
 }
 
 const anecdotes = [
@@ -34,5 +51,9 @@ const anecdotes = [
     'Premature optimization is the root of all evil.',
     'Debuggin is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart eough to debug it.']
 
+const points = new Array(anecdotes.length).fill(0);
+console.log(points)
 
-ReactDOM.render(<App anecdotes = {anecdotes}/>, document.getElementById('root'));
+
+
+ReactDOM.render(<App anecdotes = {anecdotes} points = {points}/>, document.getElementById('root'));
