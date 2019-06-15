98c4488425f01963784b6688d3040222bfa14972
Macville <manninen94@gmail.com>
2019-06-15 23:42:54 +0300

    1.14*: anekdootit step3

--- Osa1/anecdotes/src/index.js
+++ Osa1/anecdotes/src/index.js
@@ -1,55 +1,60 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
+const App = ({anecdotes, points}) => {
     const [selected, setSelected] = useState(0)
+    const [mostVoted, setMostVoted] = useState(0)
+    const [votes, setVotes] = useState(0)
 
+
+// En keksinyt miten korvata kopiolla vanha points taulukko
     const handleVote = () => {
-        props.points[selected] += 1
-        console.log(props.points)
+        points[selected] += 1
+        console.log(points)
+        let mostVoted = points.indexOf(Math.max(...points));
+        setMostVoted(mostVoted)
+        setVotes(points[selected])
+
     }
 
     const handleNext = () => {
         console.log('next anecdote clicked')
-        let nextNumber  = getRandomNumber(0, props.anecdotes.length)
+        let nextNumber  = getRandomNumber(0, anecdotes.length)
         console.log(nextNumber)
         setSelected(nextNumber)
     }
 
-    let mostVoted = points.indexOf(Math.max(...points));
-
- 
 return (
     <div>
         <h1>Anecdote of the day</h1>
-        {props.anecdotes[selected]}
+        {anecdotes[selected]}
         <br/>
-        has {props.points[selected]} votes
+        has {points[selected]} votes
         <br/>
-        <button onClick = {handleVote}> vote </button>
-        <button onClick = {handleNext}> next anecdote </button>
-
-        <h1> Anecdote with the most votes</h1>
-        {props.anecdotes[mostVoted]}
+        <button onClick = {handleVote}>vote</button>
+        <button onClick = {handleNext}>next anecdote</button>
+        <h1>Anecdote with most votes</h1>
+        {anecdotes[mostVoted]}
         <br/>
-        has {props.points[mostVoted]} points
-        
+        has {points[mostVoted]} votes
+        {}
 
     </div>
 )
 }
+
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max-min) + min);
 }
 
 const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The fist 90 precent of the code accounts for the first 90 precent of the development time... time reaining 10 precent of the code accounts for the other 90 precent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humands can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debuggin is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart eough to debug it.']
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.']
 
 const points = new Array(anecdotes.length).fill(0);
 console.log(points)
