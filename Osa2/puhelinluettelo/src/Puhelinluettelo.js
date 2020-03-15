943d4731c9e6fb0bc045e8f045edb316d1406146
Macville <manninen94@gmail.com>
2020-03-15 09:19:55 +0200

    edited puhelinluettelo

---
+++ Osa2/puhelinluettelo/src/Puhelinluettelo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  const laskuri  = (props) => {
+    console.log('laskuri called', props);
+  }
+
+  return (
+
+    
+    <div>
+      <h1>Minun oma nettisivuni :))</h1>
+     <p> IM NEW TO THIS </p>
+    </div>)
+}
+
+
