52f2a4b1f8ecdfcaca3850749e953bfc26b046c4
Ville Manninen <vw.manninen@gmail.com>
2020-07-11 00:46:15 +0300

    update

--- Osa2/kurssitiedot/src/index.js
+++ Osa2/kurssitiedot/src/index.js
@@ -1,58 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Course from './course/Course'
-import './index.css'
+import App from './App'
 
 
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-  return (
-    <div>
-      <Course courses={courses}></Course>
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
