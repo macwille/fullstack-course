43238ba28290b19ee61a257ad316849cd2a05802
Ville Manninen <vw.manninen@gmail.com>
2020-07-10 20:30:15 +0300

    fixed typos

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -1,38 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Statistics = ({feedback, good, neutral, bad}) => {
-        if (feedback === 0) {
-            return (
-                <div>
+const Statistics = ({ feedback, good, neutral, bad }) => {
+    if (feedback === 0) {
+        return (
+            <div>
                 <h2>
                     Statistics
                 </h2>
-                <p> no feedback given</p>
-                </div>
-            )
-        }
-        return (
-            <div>
-            <h2> Statistics
-
-            </h2>
-            good {good}
-            <br></br>
-            neutral  {neutral}
-            <br></br>
-            bad {bad}
-            <br></br>
-            all {feedback}
-            <br></br>
-            average {(good - bad) / feedback}
-            <br></br>
-            positive {good / feedback* 100} %
+                <p> no feedback yet.</p>
             </div>
-            
-
         )
-    
+    }
+    return (
+        <div>
+            <h2> Statistics </h2>
+
+            good {good}.
+            <br></br>
+            neutral  {neutral}.
+            <br></br>
+            bad {bad}.
+            <br></br>
+            all {feedback}.
+            <br></br>
+            average {(good - bad) / feedback}.
+            <br></br>
+            positive {good / feedback * 100} %.
+        </div>
+
+
+    )
+
 
 }
 
@@ -43,26 +42,26 @@ const App = () => {
     const [feedback, setFeedBack] = useState(0)
 
     const handleGoodClick = () => {
-        setGood(good +1)
-        setFeedBack(feedback+1)
+        setGood(good + 1)
+        setFeedBack(feedback + 1)
     }
     const handleNeutralClick = () => {
-        setNeutral(neutral +1)
-        setFeedBack(feedback+1)
+        setNeutral(neutral + 1)
+        setFeedBack(feedback + 1)
     }
     const handleBadClick = () => {
-        setBad(bad +1)
-        setFeedBack(feedback+1)
+        setBad(bad + 1)
+        setFeedBack(feedback + 1)
     }
-    
+
     return (
         <div>
-            <h1> Give feedback</h1>
-            <button onClick = {handleGoodClick}>Good</button>
-            <button onClick = {handleNeutralClick}>Neutral</button>
-            <button onClick = {handleBadClick}>Bad</button>
+            <h1> Give Feedback</h1>
+            <button onClick={handleGoodClick}>Good</button>
+            <button onClick={handleNeutralClick}>Neutral</button>
+            <button onClick={handleBadClick}>Bad</button>
 
-            <Statistics feedback = {feedback} good = {good} neutral = {neutral} bad = {bad}></Statistics>
+            <Statistics feedback={feedback} good={good} neutral={neutral} bad={bad}></Statistics>
 
         </div>
     )
