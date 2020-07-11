607cc44341ba5768d7ed33d41df2554b04131ae2
Ville Manninen <vw.manninen@gmail.com>
2020-07-11 23:41:11 +0300

    finished example Osa2

--- Osa2/notes/src/App.js
+++ Osa2/notes/src/App.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react'
 import Note from './components/Note'
 
+// Main App
 const App = (props) => {
+
    const [notes, setNotes] = useState(props.notes)
    const [newNote, setNewNote] = useState('a new note...')
+   const [showAll, setShowAll] = useState(true)
 
+   // Adding a new note
    const addNote = (event) => {
-      event.preventDefeult()
+      event.preventDefault()
       console.log('button clicked ', event.target)
       const noteObject = {
          content: newNote,
          date: new Date().toISOString,
          important: Math.random() > 0.5,
-         id: notes.length,
+         id: notes.length + 1,
       }
+      console.log('added new note: ', noteObject)
       setNotes(notes.concat(noteObject))
-      setNewNote('')
+      setNewNote('you have ' + (notes.length + 1) + ' notes...')
 
    }
 
@@ -24,22 +29,26 @@ const App = (props) => {
       setNewNote(event.target.value)
    }
 
-   const rows = () => notes.map(note =>
-      <Note
-         key={note.id}
-         note={note}
-      />
-   )
+   const notesToShow = showAll
+      ? notes
+      : notes.filter(note => note.important === true)
 
 
+   // Return body
    return (
       <div>
          <h1>Notes</h1>
+         <div>
+            <button onClick={() => setShowAll(!showAll)} >
+               show {showAll ? 'only important' : 'all'} notes
+            </button>
+         </div>
          <ul>
-            {notes.map(note =>
-               <Note key={note.id} note={note} />
+            {notesToShow.map(note =>
+               < Note key={note.id} note={note} />
             )}
          </ul>
+
          <form onSubmit={addNote}>
             <input
                value={newNote}
