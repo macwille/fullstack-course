89bceff6fb7ca48f63492b4a1965c6586e5b3c1c
Ville Manninen <vw.manninen@gmail.com>
2020-09-13 19:51:37 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- Osa4/utils/list_helper.js
+++ Osa4/utils/list_helper.js
@@ -1,7 +1,32 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1
 }
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+const favouriteBlog = (blogs) => {
+    const favourite = blogs.reduce(function (prev, current) {
+        return (prev.likes > current.likes) ? prev : current
+    })
+    return favourite
+}
+const mostBlogs = (blogs) => {
+    const freq = _.countBy(blogs, function (each) { return each });
+
+    console.log(freq)
+
+    return 1
+}
+
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favouriteBlog,
+    mostBlogs
 }
