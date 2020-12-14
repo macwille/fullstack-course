9299fdcd15a11e2fb81010be5f65ba6fc297e4ca
Ville Manninen <vw.manninen@gmail.com>
2020-12-14 09:25:31 +0200

    7.7*

--- Osa7/country-hook/src/App.js
+++ Osa7/country-hook/src/App.js
@@ -18,7 +18,18 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  //useEffect()
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+      .then(res => {
+        setCountry({ found: true, data: { ...res.data[0] } })
+      })
+      .catch((e) => {
+        console.log(e)
+        setCountry({ found: false })
+      })
+  }, [name])
 
   return country
 }
@@ -40,8 +51,8 @@ const Country = ({ country }) => {
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`} />
     </div>
   )
 }
