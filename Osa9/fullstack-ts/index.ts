9d2e91b8fee36c1011c4168f28dd650a0baf0555
macwille <vw.manninen@gmail.com>
2020-12-31 05:10:53 +0200

    9.7
    Added request folder for VS Code Rest to test POST requests

--- Osa9/fullstack-ts/index.ts
+++ Osa9/fullstack-ts/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { calculateBmi } from './calculateBmi';
-// import { calculateExcercises } from './exerciseCalculator';
+import { calculateExcercises } from './exerciseCalculator'
+
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   const helloString = 'Hello Full Stack!';
@@ -11,7 +13,6 @@ app.get('/hello', (_req, res) => {
 app.get('/bmi', (req, res) => {
   const height = Number(req.query.height);
   const weight = Number(req.query.weight);
-
   if (isNaN(height) || isNaN(weight)) {
     res.status(400).json({ error: "Malformatted Parameters" });
   }
@@ -23,15 +24,40 @@ app.get('/bmi', (req, res) => {
   };
   res.send(result);
 });
-/*
-app.get('/exercises', (req, res) => {
-  const body = req.body;
-  const list = body.daily_exercises;
-  const target = body.target;
-  const result = calculateExcercises(list, target);
-  res.send(result);
+
+app.post('/exercises', (req, res) => {
+  const getDaily = (object: { daily_exercises: Array<number> }) => {
+    if (!(req.body instanceof Object) || !('daily_exercises' in req.body)) {
+      throw new Error('Parameters missing');
+    } else {
+      return object.daily_exercises;
+    }
+  };
+  const getTarget = (object: { target: number }): number => {
+    if (!(req.body instanceof Object) || !('target' in req.body)) {
+      throw new Error('Parameters missing');
+    } else {
+      return object.target;
+    }
+  };
+  try {
+    const daily = getDaily(req.body);
+    const target = getTarget(req.body);
+    if (!Array.isArray(daily)) {
+      return res.status(400).json({ error: 'Malformated parameters' });
+    }
+    if (isNaN(Number(target)) || daily.some(isNaN)) {
+      return res.status(400).json({ error: 'Malformated parameters' });
+    }
+    if (daily.length === 0) {
+      return res.status(400).json({ error: 'Malformated parameters' });
+    }
+    return res.send(calculateExcercises(target, daily));
+  } catch (error) {
+    return res.status(400).send((error as Error).message);
+  }
 });
-*/
+
 const PORT = 3003;
 
 app.listen(PORT, () => {
