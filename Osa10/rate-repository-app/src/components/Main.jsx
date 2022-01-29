56802187929b649eee39f6efd7bff57a6ed52dfe
macwille <vw.manninen@gmail.com>
2022-01-29 12:33:40 +0200

    10.5

--- Osa10/rate-repository-app/src/components/Main.jsx
+++ Osa10/rate-repository-app/src/components/Main.jsx
@@ -1,18 +1,20 @@
 import Constants from 'expo-constants';
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AppBar from './AppBar';
 import RepositoryList from './RepositoryList';
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: "#e1e4e8"
   },
 });
 
 const Main = () => {
   return (
     <View style={styles.container}>
+      <AppBar />
       <RepositoryList />
     </View>
   );
--- Osa10/rate-repository-app/src/components/RepositoryItem.jsx
+++ Osa10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,5 +1,56 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View, StyleSheet, Image } from "react-native";
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: Constants.statusBarHeight,
+    minHeight: "15%",
+    backgroundColor: "#fff",
+  },
+  flexBox: {
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 5,
+  },
+  flexBoxItem: {
+    padding: "4%"
+  },
+  headerText: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  subtitleText: {
+    textAlign: "center",
+  },
+  boldText: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  languageText: {
+    backgroundColor: "#3399ff",
+    padding: 3,
+    margin: 10,
+    marginLeft: "30%",
+    marginRight: "30%",
+    color: "#fff",
+    borderRadius: 8,
+    textAlign: "center"
+  },
+  avatarImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 10,
+    marginLeft: "auto",
+    marginRight: "auto",
+  }
+});
+
+// Rounds a number larger than 999 to the closest 100 mark and adds the suffix "k".
+const thousandRounder = (number) => {
+  return Math.abs(number) > 999 ? Math.sign(number) * ((Math.abs(number) / 1000).toFixed(1)) + 'k' : Math.sign(number) * Math.abs(number)
+}
 
 const RepositoryItem = ({
   fullName,
@@ -10,18 +61,36 @@ const RepositoryItem = ({
   ratingAverage,
   reviewCount,
   ownerAvatarUrl
+
 }) => {
 
   return (
-    <View>
-      <Text>Full name: {fullName}</Text>
-      <Text>Description: {description}</Text>
-      <Text>Language: {language}</Text>
-      <Text>Stars: {stargazersCount}</Text>
-      <Text>Forks: {forksCount}</Text>
-      <Text>Reviews: {reviewCount}</Text>
-      <Text>Rating: {ratingAverage}</Text>
-    </View>
+    <View style={styles.container}>
+      <Image style={styles.avatarImage} source=
+        {{ uri: ownerAvatarUrl }}
+      />
+      <Text style={styles.headerText}>{fullName}</Text>
+      <Text style={styles.subtitleText}>{description}</Text>
+      <Text style={styles.languageText}>{language}</Text>
+      <View style={styles.flexBox}>
+        <View style={styles.flexBoxItem}>
+          <Text>Stars</Text>
+          <Text style={styles.boldText} > {thousandRounder(stargazersCount)}</Text>
+        </View>
+        <View style={styles.flexBoxItem}>
+          <Text>Forks</Text>
+          <Text style={styles.boldText} >{thousandRounder(forksCount)}</Text>
+        </View>
+        <View style={styles.flexBoxItem}>
+          <Text>Reviews</Text>
+          <Text style={styles.boldText} >{thousandRounder(reviewCount)}</Text>
+        </View>
+        <View style={styles.flexBoxItem}>
+          <Text>Rating</Text>
+          <Text style={styles.boldText} >{ratingAverage}</Text>
+        </View>
+      </View>
+    </View >
   )
 }
 
