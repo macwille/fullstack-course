eed8a7443bc1c24f6dfaddd357a0c85dd4083413
macwille <vw.manninen@gmail.com>
2022-01-29 11:28:18 +0200

    10.3.

---
+++ Osa10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const RepositoryItem = ({
+  fullName,
+  description,
+  language,
+  forksCount,
+  stargazersCount,
+  ratingAverage,
+  reviewCount,
+  ownerAvatarUrl
+}) => {
+
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  )
+}
+
+export default RepositoryItem;
