dd624a47a4a35f586be2d129d8a1d299d1b8ec58
macwille <vw.manninen@gmail.com>
2022-01-29 10:26:41 +0200

    Update rate-repository-app

--- Osa10/rate-repository-app/App.js
+++ Osa10/rate-repository-app/App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -10,15 +18,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-
-export default App;
-
