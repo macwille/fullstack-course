0ba4e3e47ea38d5c4ed64023ac12770be1aab5e4
macwille <vw.manninen@gmail.com>
2022-02-02 13:17:39 +0200

    10.12

--- Osa10/rate-repository-app/src/utils/apolloClient.js
+++ Osa10/rate-repository-app/src/utils/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.37:4000/graphql',
+  uri: `${Constants.manifest.extra.uri}:4000/graphql`,
 });
 
 const createApolloClient = () => {
